27b261508d05e3509ecfed47b426f1495cb48f2e
Slavvvvvva <jurik199307@gmail.com>
2021-03-31 00:07:56 +0200

    add Homepage, menu-item, styles

---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Directory from '../../Components/Directory/Directory'
+
+import './homepage.styles.scss'
+
+
+const HomePage = (props) => (
+
+    <div className = 'homepage'>
+        <Directory/>
+    </div>
+
+)
+export default HomePage

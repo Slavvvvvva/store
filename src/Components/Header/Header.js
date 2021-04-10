ddac0222e65e91678de297f4ca88fc00d583091f
Slavvvvvva <jurik199307@gmail.com>
2021-04-10 20:04:25 +0200

    implemented card reduser

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,7 +9,7 @@ import CartDropdown from '../CartDropdown/CartDropdown'
 
 
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     
     <div className='header'>
         <Link className='logo-container' to='/'>
@@ -25,11 +25,12 @@ const Header = ({currentUser}) => (
             }
             <CartIcon/>
         </div>
-        <CartDropdown/>
+        {hidden? null : <CartDropdown/>}
     </div>
 )
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser}, cart:{hidden} }) => ({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header)
---
+++ src/Redux/cart/cart-action.js
@@ -0,0 +1,4 @@
+import {CartActionTypes} from './cart-action-types'
+export const toggleCartHidden = () => ({
+    type: CartActionTypes.TOGGLE_CART_HIDDEN,
+})

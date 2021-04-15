ab69a32fdd4c85b700e25a2820cc2dcba7986695
Slavvvvvva <jurik199307@gmail.com>
2021-04-15 20:20:14 +0200

    added addItmem and removeItem to checkout item component

--- src/Components/CheckoutItem/ChackoutItem.js
+++ src/Components/CheckoutItem/ChackoutItem.js
@@ -1,19 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { addItem, clerarItemFromCart, removeItem } from '../../Redux/cart/cart-action'
 import './checkout-item.styles.scss'
 
-const ChekoutItem = ({cartItem:{name, imageUrl, price, quantity}}) => {
+const ChekoutItem = ({cartItem, clearItem,addItem, removeItem}) => {
+    const {name, imageUrl, price, quantity} = cartItem
     return (
         <div className='checkout-item'>
             <div className ='image-container'>
                 <img src = {imageUrl} alt ='item' />
             </div>
             <span className ='name'>{name}</span>
-            <span className ='quantity'>{quantity}</span>
+            <span className ='quantity'>
+            <div className ='arrow' onClick= {()=> removeItem(cartItem)}>&#10094;</div>
+            <span className = 'value'>{quantity}</span>
+            <div className ='arrow' onClick ={() => addItem(cartItem)}> &#10095; </div>
+            </span>
             <span className ='price'>{price}</span>
-            <div className ='remove-button'> &#10005;</div>
+            <div className ='remove-button' onClick ={ () => clearItem(cartItem)}> &#10005;</div>
         </div>
     )
 
 }
 
-export default ChekoutItem
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clerarItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(ChekoutItem)
--- src/Redux/cart/cart-action.js
+++ src/Redux/cart/cart-action.js
@@ -1,4 +1,5 @@
 import {CartActionTypes} from './cart-action-types'
+
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN,
 })
@@ -6,4 +7,14 @@ export const toggleCartHidden = () => ({
 export const addItem = (payload) => ({
     type: CartActionTypes.ADD_ITEM,
     payload
+})
+
+export const removeItem = (payload) => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload
+})
+
+export const clerarItemFromCart = item => ({
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
 })

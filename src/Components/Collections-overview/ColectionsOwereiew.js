802839ff02cead30fd0616ab9dcf4f340b8ac43d
Slavvvvvva <jurik199307@gmail.com>
2021-04-17 15:29:49 +0200

    update shop data, shop selectors, add adwanced routing

--- src/Components/Collections-overview/ColectionsOwereiew.js
+++ src/Components/Collections-overview/ColectionsOwereiew.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { selectShopCollections } from '../../Redux/shop/shop-selectors'
+import { selectCollectionsForPreview } from '../../Redux/shop/shop-selectors'
 import PreviewColection from '../PreviewColection/PreviewColection'
 import './collections-overview.styles.scss'
 
@@ -20,7 +20,7 @@ const CollectionsOverview  = ({collections}) => {
 }
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
--- src/Pages/ShopPage/ShopPage.js
+++ src/Pages/ShopPage/ShopPage.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Route } from 'react-router'
 import ColectionsOwereiew from '../../Components/Collections-overview/ColectionsOwereiew'
+import CollectionPage from '../CollectionPage/CollectionPage'
 
-const ShopPage = ({collections}) => {
-
+const ShopPage = ({match}) => {
+    
     return (
         <div className='shop-page'>
-            <ColectionsOwereiew/>
+            <Route exact path ={`${match.path}`} component = {ColectionsOwereiew}/>
+            <Route  path ={`${match.path}/:collectionId`} component = {CollectionPage}/>      
         </div>
     )
 
